0ac8881a34dc92f9868afea2314fbbdb6c84a48e
tomas <tomasroma64@gmail.com>
2025-02-01 21:41:06 +0000

    favicon and title n name

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 
 import JoinClassroom from '@/components/JoinClassroom'
 import Layout from '@/components/Layout'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <Layout>
+      <Image src="/logo.svg" alt="BlockDraw" width={100} height={100} />
       <JoinClassroom />
     </Layout>
   )
