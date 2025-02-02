9ecc1bb0dbb57b671f64b56d6ab63c9fc4021720
tomas <tomasroma64@gmail.com>
2025-02-02 09:10:41 +0000

    oops

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import JoinClassroom from '@/components/JoinClassroom'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-h-[calc(100dvh)] bg-blue-500 gap-12">
+    <div className="flex flex-col items-center justify-center h-[calc(100dvh)] bg-blue-500 gap-12">
       <JoinClassroom />
     </div>
   )
